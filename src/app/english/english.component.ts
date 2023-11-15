240e5c0a2530417dc0b3d0a50a61fdfb01fbf99c
iphadan <ephadaniel177@gmail.com>
2023-11-15 09:35:31 +0300

    Joint Form improved

--- src/app/english/english.component.ts
+++ src/app/english/english.component.ts
@@ -18,106 +18,65 @@ export class EnglishComponent {
   title = 'PrintKYC';
   formFields: { [key: string]: string } = {};
   language="English";
-
+  formNumber:number=0
+  formNumberArray=Array(this.formNumber).fill(0)
+ photoURLArray=Array(this.formNumber).fill(0)
 
   selectedFile: any;
   imageURL: string = "";
-  previewVisible = false; // Indicates whether the preview is visible
-  pdfPreviewUrl!: string; // Holds the URL of the PDF preview
+  
 
   hideFileInput = false;
   hideJoint = true;
   
-  previewPDF() {
-
-    const doc = new jsPDF();
-    const formFields = this.kycForm.value;
-    console.log('preview', formFields)
-
-    // Set the font size and position for the first field
-    let fontSize = 12;
-    let positionX = 20;
-    let positionY = 20;
-
-    // Iterate over the form fields and add them to the PDF
-    for (const fieldName in formFields) {
-      if (formFields.hasOwnProperty(fieldName)) {
-        const fieldValue = formFields[fieldName];
-        doc.setFontSize(fontSize);
-        doc.text(`${fieldName}: ${fieldValue}`, positionX, positionY);
-        positionY += 10; // Increase the Y position for the next field
-      }
-    }
-
-    // Generate the PDF data URL for preview
-    const pdfDataUri = doc.output('datauristring');
-
-    // Set the preview URL and make it visible
-    this.pdfPreviewUrl = pdfDataUri;
-    this.previewVisible = true;
-  }
+ 
 
 
-  @ViewChild('content', { static: false }) content!: ElementRef
-
-
-
-  public async Download() {
-    window.print();
-  }
-
-
-  generatePDF() {
-    const contentElement = this.content.nativeElement;
-
-
-    ;
-    html2canvas(contentElement).then((canvas) => {
-      const imgWidth = 208;
-      const imgHeight = 250
-
-      const contentDataURL = canvas.toDataURL('image/png');
-      const pdf = new jsPDF('p', 'mm', 'a4');
-
-      let position = 0;
-      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
-      pdf.save('test.pdf');
-
-    });
-  }
+ 
 
 
 
 
-  onFileSelected(event: any) {
+  onFileSelected(event: any,i:number) {
     if (event.target.files && event.target.files[0]) {
+      
       const file = event.target.files[0];
       this.selectedFile = file;
       const reader = new FileReader();
-      reader.onload = (e) => (this.imageURL = reader.result as string);
+
+  
+      reader.onload = (e) => (this.photoURLArray[i]= reader.result as string);
 
       reader.readAsDataURL(file);
     }
   }
 onSelect(event:any){
   this.language=event.target.value;
-  console.log(this.language)
+
 }
 
-  onChangeButton() {
+  onChangeButton(formNumber : String) {
+    if (Number(formNumber) && this.hideJoint == true && Number(formNumber) >0){
+this.hideJoint=false
+    }
+  else {
+    this.hideJoint=true
+  }
+    this.formNumberArray=Array(0).fill(0)
+    let i:number;
+this.formNumber=Number(formNumber);
+for(i = 1;i<=this.formNumber;i++) {
 
-    if (this.hideJoint === false ) {
-      this.hideJoint = true;
+  this.formNumberArray.push(i+1);
+  this.photoURLArray.push("");
+  console.log(i)
   
+ 
+}
+this.formNumber=0;
 
-    
+console.log(this.formNumberArray)
 
-      
-    }
-    else {
-      this.hideJoint = false;
-
-    }
 
   }
 
